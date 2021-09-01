e471865599a60b16c888358290403d30f9c9417b
nirmal0806 <sainirmal008@gmail.com>
2021-09-01 13:23:31 -0500

    GG

---
+++ test_one.java
@@ -0,0 +1,8 @@
+public class test_one {
+     public static void main(String[] args) {
+         System.out.println("practicing Git is tough");
+         System.out.println("Its Wednesday today");
+        
+        
+    }
+}
