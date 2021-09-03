56a6d12e347dc576d43b92e350caa536ff7eb182
nirmal0806 <sainirmal008@gmail.com>
2021-09-03 14:08:06 -0500

    changes done

--- test_one.java
+++
@@ -1,8 +0,0 @@
-public class test_one {
-     public static void main(String[] args) {
-         System.out.println("practicing Git is tough");
-         System.out.println("Its Wednesday today");
-        
-        
-    }
-}
